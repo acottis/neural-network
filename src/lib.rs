e70c6bb375317bc40c95ee36b3645c1211fcf401
Adam <adam>
2023-09-06 01:22:20 +0100

    User interface, refactor, documentation

--- src/lib.rs
+++ src/lib.rs
@@ -10,64 +10,80 @@ struct Neuron {
 
 #[derive(Debug)]
 struct Layer {
+    /// A collection of Weights and Biases, each neuron has the same number of weights as a layer
+    /// has inputs
     neurons: Vec<Neuron>,
+
+    /// The inputs to the current layer before any operations
     inputs: Vec<f64>,
+
+    /// The outputs of a layer before activation function is applied
     outputs: Vec<f64>,
+
+    /// The activation function that is applied on this layer
     activation: Activation,
 }
 
 impl Layer {
-    fn new(rng: &mut StdRng, inputs_len: usize, neurons: usize, activation: Activation) -> Self {
-        let mut layer = Vec::with_capacity(neurons);
+    fn new(rng: &mut StdRng, inputs: usize, neurons: usize, activation: Activation) -> Self {
+        let mut neurons = Vec::with_capacity(neurons);
 
-        for _ in 0..neurons {
-            let mut weights = Vec::with_capacity(inputs_len);
-            for _ in 0..inputs_len {
+        for _ in 0..neurons.capacity() {
+            let mut weights = Vec::with_capacity(inputs);
+
+            for _ in 0..inputs {
                 weights.push(rng.gen());
             }
-
-            layer.push(Neuron { weights, bias: 0.0 });
+            neurons.push(Neuron { weights, bias: 0.0 });
         }
 
         Self {
-            neurons: layer,
-            inputs: Vec::with_capacity(inputs_len),
-            outputs: vec![0.0; inputs_len],
+            neurons,
+            inputs: Vec::with_capacity(inputs),
+            outputs: vec![0.0; inputs],
             activation,
         }
     }
 
+    /// This is the hot loop
     fn forward_pass(&mut self, inputs: &Vec<f64>) -> Vec<f64> {
         // TODO: This needs to go
-        let mut activated_outputs = Vec::with_capacity(self.neurons.len());
+        let mut layer_outputs = Vec::with_capacity(self.neurons.len());
 
         // Save our inputs for backward propagation
         self.inputs.clear();
-        self.inputs.extend_from_slice(&inputs);
+        self.inputs.extend(inputs);
 
         for (neuron, output) in self.neurons.iter().zip(self.outputs.iter_mut()) {
+            // Since we are adding below we need to reset output to 0.0 as we are mutating
+            // an exisiting value
             *output = 0.0;
+
             // For every weight and every input dot product
             for (weight, input) in neuron.weights.iter().zip(inputs.iter()) {
                 *output += weight * input;
             }
-            // Save our output before activation
+
+            // Save our output before activation for use in cost calculation
             *output += neuron.bias;
-            activated_outputs.push(self.activation.function(*output));
+
+            // Activate our neuron and add to our return
+            layer_outputs.push((self.activation.function)(*output));
         }
-        activated_outputs
+        layer_outputs
     }
 
+    /// Calculate our errors and update weights/biases
     fn backward_pass(&mut self, learning_rate: f64, mut errors: Vec<f64>) -> Vec<f64> {
         let deltas: Vec<f64> = self
             .outputs
             .iter()
             .zip(errors.iter())
-            .map(|(output, error)| error * self.activation.derivative(*output))
+            .map(|(output, error)| error * (self.activation.derivative)(*output))
             .collect();
 
         // Errors no longer needed so we re-init
-        errors = Vec::with_capacity(self.inputs.len());
+        errors.clear();
 
         for (neuron, (delta, input)) in self
             .neurons
@@ -92,23 +108,33 @@ impl Layer {
 pub struct NeuralNetwork {
     layers: Vec<Layer>,
     learning_rate: f64,
+    inputs: usize,
+    rng: StdRng,
 }
 
 impl NeuralNetwork {
-    pub fn new() -> Self {
+    pub fn new(inputs: usize) -> Self {
         let learning_rate = 0.5;
-        let mut rng = StdRng::seed_from_u64(1337);
-
-        let layers = vec![
-            Layer::new(&mut rng, 1, 10, Activation::Sigmoid),
-            Layer::new(&mut rng, 10, 10, Activation::Sigmoid),
-            Layer::new(&mut rng, 10, 1, Activation::Sigmoid),
-        ];
+        let rng = StdRng::seed_from_u64(1337);
 
         Self {
-            layers,
+            layers: Vec::new(),
             learning_rate,
+            rng,
+            inputs,
         }
+    }
+
+    pub fn layer(mut self, neurons: usize, activation: Activation) -> Self {
+        let inputs = match self.layers.last() {
+            Some(layer) => layer.neurons.len(),
+            None => self.inputs,
+        };
+
+        self.layers
+            .push(Layer::new(&mut self.rng, inputs, neurons, activation));
+
+        self
     }
 
     pub fn train(
@@ -117,27 +143,32 @@ impl NeuralNetwork {
         training_targets: Vec<Vec<f64>>,
         epochs: usize,
     ) {
+        if self.layers.len() == 0 {
+            panic!("No layers added")
+        }
         for epoch in 0..epochs {
             for (inputs, targets) in training_inputs.iter().zip(training_targets.iter()) {
+                // Get the prediction for the training run
                 let outputs = self.feed_forward(inputs.clone());
 
                 if epoch % 100 == 0 {
                     println!(
-                        "Epoch: {epoch} inputs: {:?}, predictions: {:?}",
-                        &inputs, &outputs
+                        "Epoch: {} inputs: {:?}, predictions: {:?}",
+                        epoch, &inputs, &outputs
                     );
                 }
 
+                // Update our weights based on how far prediction is from expected
                 self.back_propagate(&outputs, &targets);
             }
         }
     }
 
     fn back_propagate(&mut self, outputs: &Vec<f64>, targets: &Vec<f64>) {
-        let mut errors: Vec<f64> = targets
+        let mut errors: Vec<f64> = outputs
             .iter()
-            .zip(outputs.iter())
-            .map(|(target, output)| output - target)
+            .zip(targets.iter())
+            .map(|(output, target)| output - target)
             .collect();
 
         for layer in self.layers.iter_mut().rev() {
@@ -154,27 +185,21 @@ impl NeuralNetwork {
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug)]
-enum Activation {
-    Sigmoid,
-    Identity,
+pub struct Activation {
+    function: fn(f64) -> f64,
+    derivative: fn(f64) -> f64,
 }
 
-impl Activation {
-    fn function(&self, x: f64) -> f64 {
-        match self {
-            Self::Sigmoid => 1.0 / (1.0 + E.powf(-x)),
-            Self::Identity => x * 1.0,
-        }
-    }
-    fn derivative(&self, x: f64) -> f64 {
-        match self {
-            Self::Sigmoid => self.function(x) * (1.0 - self.function(x)),
-            Self::Identity => 1.0,
-        }
-    }
-}
+pub const SIGMOID: Activation = Activation {
+    function: |x| 1.0 / (1.0 + E.powf(-x)),
+    derivative: |x| (SIGMOID.function)(x) * (1.0 - (SIGMOID.function)(x)),
+};
+
+pub const IDENTITY: Activation = Activation {
+    function: |x| x * 1.0,
+    derivative: |_| 1.0,
+};
 
 #[cfg(test)]
 mod tests {
@@ -182,7 +207,7 @@ mod tests {
 
     #[test]
     fn one_sized_nn() {
-        let mut nn = NeuralNetwork::new();
+        let mut nn = NeuralNetwork::new(1).layer(5, SIGMOID).layer(1, SIGMOID);
 
         let inputs = vec![vec![1.0], vec![0.0]];
         let targets = vec![vec![1.0], vec![0.0]];
